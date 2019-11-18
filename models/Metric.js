8b2d186637081409063298315956af1ecbf7c895
Franz Hoepfner <kinnarr@hollywuuud.de>
2019-11-18 16:30:11 +0100

    Adds database settings to Metric model

--- models/Metric.js
+++ models/Metric.js
@@ -12,8 +12,12 @@ export default class Metric extends Model {
       unit: this.string().nullable(),
       source: this.string().nullable(),
       historic: this.boolean(false),
+      // Database settings
       databaseId: this.string().nullable(),
       database: this.belongsTo(Database, 'databaseId'),
+      intervalMin: this.string().nullable(),
+      intervalMax: this.string().nullable(),
+      intervalFactor: this.number().nullable(),
       // Client side stat
       selected: this.boolean(false)
     }
