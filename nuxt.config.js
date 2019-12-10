d394d01ac2b5ab72c7aeb83313bec528eb50ba64
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2019-12-10 14:10:04 +0100

    Add new logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }]
   },
   /*
    ** Customize the progress-bar color
