749f431ff352b6a6f92be2eb25a206338c15f975
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2020-03-11 09:06:15 +0100

    Stick navbar to top and add transformer spinner

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,64 +1,67 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" sticky fixed="top">
-      <b-navbar-brand :to="{ name: 'index' }">
-        MetricQ &#x1F9D9; ‍️
-      </b-navbar-brand>
+  <b-navbar toggleable="lg" sticky fixed="top" variant="light">
+    <b-navbar-brand :to="{ name: 'index' }">
+      MetricQ &#x1F9D9; ‍️
+    </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse" />
+    <b-navbar-toggle target="nav-collapse" />
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item
-            :to="{
-              name: 'metric-metric_list'
-            }"
-            active-class="active"
-          >
-            Metrics Overview
-          </b-nav-item>
-          <b-nav-item
-            :to="{
-              name: 'source-source_list'
-            }"
-            active-class="active"
-          >
-            Source Overview
-          </b-nav-item>
-          <b-nav-item
-            :to="{
-              name: 'client-client_list'
-            }"
-            active-class="active"
-          >
-            Client Overview
-          </b-nav-item>
-        </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item
+          :to="{
+            name: 'metric-metric_list'
+          }"
+          active-class="active"
+        >
+          Metrics Overview
+        </b-nav-item>
+        <b-nav-item
+          :to="{
+            name: 'source-source_list'
+          }"
+          active-class="active"
+        >
+          Source Overview
+        </b-nav-item>
+        <b-nav-item
+          :to="{
+            name: 'client-client_list'
+          }"
+          active-class="active"
+        >
+          Client Overview
+        </b-nav-item>
+      </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-text v-if="fetchingMetrics" class="mr-sm-2">
-            <b-spinner class="ml-auto" small />
-            <strong>Fetching metrics...</strong>
-          </b-nav-text>
-          <b-nav-text v-if="fetchingDatabases" class="mr-sm-2">
-            <b-spinner class="ml-auto" small />
-            <strong>Fetching databases...</strong>
-          </b-nav-text>
-          <b-nav-text v-if="fetchingSources" class="mr-sm-2">
-            <b-spinner class="ml-auto" small />
-            <strong>Fetching sources...</strong>
-          </b-nav-text>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-text v-if="fetchingMetrics" class="mr-sm-2">
+          <b-spinner class="ml-auto" small />
+          <strong>Fetching metrics...</strong>
+        </b-nav-text>
+        <b-nav-text v-if="fetchingDatabases" class="mr-sm-2">
+          <b-spinner class="ml-auto" small />
+          <strong>Fetching databases...</strong>
+        </b-nav-text>
+        <b-nav-text v-if="fetchingSources" class="mr-sm-2">
+          <b-spinner class="ml-auto" small />
+          <strong>Fetching sources...</strong>
+        </b-nav-text>
+        <b-nav-text v-if="fetchingTransformers" class="mr-sm-2">
+          <b-spinner class="ml-auto" small />
+          <strong>Fetching transformers...</strong>
+        </b-nav-text>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 import Metric from '~/models/Metric'
 import Database from '~/models/Database'
 import Source from '~/models/Source'
+import Transformer from '~/models/Transformer'
 
 export default {
   name: 'Navbar',
@@ -71,6 +74,9 @@ export default {
     },
     fetchingSources() {
       return Source.store().state.entities.source.fetching
+    },
+    fetchingTransformers() {
+      return Transformer.store().state.entities.transformer.fetching
     }
   }
 }
