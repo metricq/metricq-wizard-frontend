12dc2ecfdfc70a56188744acb65944b639b1d86b
Mario Bielert <mario.bielert@tu-dresden.de>
2023-02-10 14:33:34 +0100

    ui: humanized rate in metric workshop table (#68)
    * ui: humanized rate in metric workshop table

--- plugins/filters.js
+++ plugins/filters.js
@@ -18,3 +18,22 @@ Vue.filter('momentDuration', function (duration) {
     return moment.duration(duration, 'seconds').humanize()
   }
 })
+
+Vue.filter('humanizeRate', function (rate) {
+  // get a rate as Number and return a human-readable
+  // string representation of that rate
+
+  const interval = moment.duration(1 / rate, 'seconds')
+
+  if (interval.asHours() >= 1) {
+    return `1 / ${interval.asHours()} h`
+  } else if (interval.asMinutes() >= 1) {
+    return `1 / ${interval.asMinutes()} min`
+  } else if (interval.asSeconds() > 1) {
+    return `1 / ${interval.asSeconds()} s`
+  } else {
+    // JavaScript is weird. This abomination prints floats
+    // with at most two fractional digits, but no trailing zeros.
+    return `${parseFloat(rate.toFixed(2))} Hz`
+  }
+})
